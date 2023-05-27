a6c8d4c10918f6c56ff00a8388cf48298227bd63
Doni Nurramdan <nurramdandoni@gmail.com>
2023-05-27 12:13:21 +0700

    add middleware secutiry access

--- server.js
+++ server.js
@@ -19,16 +19,24 @@ app.use(express.json());
 // Menggunakan middleware pembatasan tingkat permintaan secara umum
 app.use(limiter);
 
+
 // ----------------------------------------------------------------- start Middleware BLock -------------------------------------------------------
 // Middleware untuk memeriksa validitas token pada setiap permintaan yang memerlukan otentikasi
 function authenticateToken(req, res, next) {
   const token = req.headers['authorization'];
-  
-  if (!token) return res.sendStatus(401);
+  const responseAutenticate = {
+    status:"Error",
+    message:"Unauthorized"
+  }
+  const responseForbiden = {
+    status:"Error",
+    message:"Invalid Token"
+  }
+  if (!token) return res.status(401).json(responseAutenticate);
   
   const validToken = token.split(" ");
   jwt.verify(validToken[1], secretKey, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.status(403).json(responseForbiden);
 
     req.user = user;
     next();
@@ -36,33 +44,46 @@ function authenticateToken(req, res, next) {
 }
 // ----------------------------------------------------------------- end Middleware BLock -------------------------------------------------------
 
+
+// ----------------------------------------------------------------- start Controller BLock -------------------------------------------------------
 const produkController = require('./controllers/produk');
-// ----------------------------------------------------------------- start SQL BLock -------------------------------------------------------
-// Endpoint GET untuk mendapatkan data pengguna
+// ----------------------------------------------------------------- end Controller BLock -------------------------------------------------------
+
+
+
+// ----------------------------------------------------------------- start API BLock -------------------------------------------------------
+
+
 app.get('/users', (req, res) => {
   connection.query('SELECT * FROM users', (err, rows) => {
     if (err) throw err;
     res.json(rows);
   });
 });
-// ----------------------------------------------------------------- end SQL Block --------------------------------------------------------
 
-
-// ----------------------------------------------------------------- start No SQL Block ---------------------------------------------------
-
-  app.get('/usersMongo', async (req, res) => {
-  const dataUser = await getUsers();
-  res.json(dataUser);
+app.get('/', (req, res) => {
+  res.send('Server berjalan dengan baik!');
 });
 
+// Endpoint untuk login dan menghasilkan token JWT
+app.post('/login',limiter, (req, res) => {
+const { username, password } = req.body;
+
+// Proses otentikasi pengguna
+// ...
+
+// Jika otentikasi berhasil, membuat token JWT
+const token = jwt.sign({ username }, secretKey,{ expiresIn: '1d' });
+res.json({ token });
+});
 // api produk master saat scan
-app.post('/produk',miniter, produkController.createProduk);
-app.post('/bulkProduk', produkController.createProdukByCsv);
-app.get('/produk',limiter, produkController.searchProduk);
-app.get('/produk/:idbarcode',limiter, produkController.searchProdukByBarcode);
-app.post('/produkNameSearch',limiter, produkController.searchProdukByName);
-app.put('/produk', produkController.updateProduk);
-app.delete('/produk', produkController.deleteProduk);
+app.post('/produk',limiter,authenticateToken, produkController.createProduk);
+app.post('/bulkProduk', authenticateToken, produkController.createProdukByCsv);
+app.get('/produk',limiter, authenticateToken, produkController.searchProduk);
+app.get('/produk/:idbarcode',limiter, authenticateToken, produkController.searchProdukByBarcode);
+app.post('/produkNameSearch',limiter, authenticateToken, produkController.searchProdukByName);
+app.put('/produk', authenticateToken, produkController.updateProduk);
+app.delete('/produk', authenticateToken, produkController.deleteProduk);
 
 app.post('/usersMongo', async (req, res) => {
   const { name, email, age } = req.body;
@@ -78,32 +99,7 @@ app.post('/usersMongo', async (req, res) => {
   }
 });
 
-// ----------------------------------------------------------------- end No SQL Block ---------------------------------------------------
-
-
-app.get('/', (req, res) => {
-    res.send('Server berjalan dengan baik!');
-  });
-
-// Endpoint untuk login dan menghasilkan token JWT
-app.post('/login',limiter, (req, res) => {
-  const { username, password } = req.body;
-
-  // Proses otentikasi pengguna
-  // ...
-
-  // Jika otentikasi berhasil, membuat token JWT
-  const token = jwt.sign({ username }, secretKey,{ expiresIn: '1d' });
-  res.json({ token });
-});
-
-
-// Contoh penggunaan middleware authenticateToken pada endpoint yang memerlukan otentikasi
-app.get('/protected', authenticateToken, (req, res) => {
-  // Hanya pengguna yang telah diotentikasi yang bisa mengakses endpoint ini
-  // ...
-  res.send('Welcome!');
-});
+// ----------------------------------------------------------------- end API Block ---------------------------------------------------
 
 // Menjalankan server
 app.listen(port, () => {
