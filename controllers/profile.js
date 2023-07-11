b07aa9eeeb121eeffdc120019bcafc52abb6c8b5
Doni Nurramdan <nurramdandoni@gmail.com>
2023-07-11 15:26:35 +0700

    add profile Controller pu

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,4 +1,4 @@
-const { findProfile } = require('../models/sql/profileModel'); // Core API
+const { findProfile, updateProfile } = require('../models/sql/profileModel'); // Core API
 
 const response500 = {
     status:"Error",
@@ -12,6 +12,34 @@ exports.findProfile = async(req, res) => {
 
     try{
         const dataProfile = await findProfile(profile_id);
+
+        if(dataProfile.status == "Sukses"){
+            const response = {
+                status:dataProfile.status,
+                message:dataProfile.message,
+                data:dataProfile.data
+            }
+            res.json(response);
+        }else{
+            const response = {
+                status:dataProfile.status,
+                message:dataProfile.message,
+                data:dataProfile.data
+              }
+              res.status(404).json(response);
+        }
+
+    }catch(error){
+        res.status(500).json(response500);
+    }
+    
+};
+exports.updateProfile = async(req, res) => {
+    const profile_id = req.params.profile_id;
+    const profile_data = req.body;
+
+    try{
+        const dataProfile = await updateProfile(profile_id,profile_data);
 
         if(dataProfile.status == "Sukses"){
             const response = {
