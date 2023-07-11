8a33e58cb5fcf0d595df7171ffa83c08c842d8d7
Doni Nurramdan <nurramdandoni@gmail.com>
2023-07-11 13:08:35 +0700

    mongodb move server

--- config/db_mongo.js
+++ config/db_mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // Koneksi ke database mongoDB
 // const url = 'mongodb://localhost:27017/payliteCore'; // local global
 // const url = 'mongodb://adminPaylite:rahasiaPayllite2022@103.82.93.30:27017/admin'; // admin api local global
-const url = 'mongodb://corePaylite:rahasiaPayllite2022@103.82.93.30:27017/payliteCore'; // api global
+const url = 'mongodb://corePaylite:rahasiaPayllite2022@103.186.1.222:27017/payliteCore'; // api global
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Terhubung ke MongoDB');
