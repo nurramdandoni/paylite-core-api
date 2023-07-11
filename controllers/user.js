0b6aabca424097ea611ee81f979deb0025e4271e
Doni Nurramdan <nurramdandoni@gmail.com>
2023-07-11 13:09:55 +0700

    update user model and controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,8 @@
 const jwtLib = require('../config/jwt');
 const crypto = require('crypto');
 
-const { loginProses } = require('../models/sql/userModel'); // Core API
+const { loginProses, loginOauth2, insertUser } = require('../models/sql/userModel'); // Core API
+const { insertProfile } = require('../models/sql/profileModel'); // Core API
 
 const response500 = {
     status:"Error",
@@ -11,29 +12,71 @@ const response500 = {
 
 // user login
 exports.searchUser = async(req, res) => {
-    const { username, password } = req.body;
+    const data = req.body;
+    const username = data.username
+    console.log("request",data);
 
-    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const hashedPassword = crypto.createHash('sha1').update(data.password).digest('hex');
     try{
-        const dataUser = await loginProses(username, hashedPassword);
+        let dataUser;
+        if(data.signWithGoogle){
+
+            dataUser = await loginOauth2(data.username);
+            console.log("kesini");
+        }else{
+            
+            dataUser = await loginProses(data.username, hashedPassword);
+        }
 
         if(dataUser.status == "Sukses"){
+            // data dari Oauth2 digunakan untuk :
+            // update profile terbaru setiap login nama, email, lokasi
+
             // Jika otentikasi berhasil, membuat token JWT
             const token = jwtLib.jwt.sign({ username }, jwtLib.secretKey,{ expiresIn: '1d' });
             const response = {
                 status:dataUser.status,
                 message:dataUser.message,
-                data:dataUser.data,
-                token:token
+                data:{
+                    user_id:dataUser.data.user_id,
+                    username:data.username,
+                    role:data.role,
+                    profile_id:dataUser.data.profile_id,
+                    token:token
+
+                }
             }
             res.json(response);
         }else{
+            // data dari Oauth2 digunakan untuk :
+            // membuat profile baru
+            const dataProfile = await insertProfile(data);
+            console.log(dataProfile.data);
+            const newUser = {
+                username:data.username,
+                password:hashedPassword,
+                role:data.role,
+                profile_id:dataProfile.data.profile_id
+            }
+            console.log("new User : ",newUser);
+            const dataUser = await insertUser(newUser);
+            // membuat user baru
+            // Jika registrasi berhasil, membuat token JWT
+            const token = jwtLib.jwt.sign({ username }, jwtLib.secretKey,{ expiresIn: '1d' });
             const response = {
                 status:dataUser.status,
-                message:dataUser.message,
-                data:dataUser.data
+                message:"Login Berhasil!",
+                data:{
+                    user_id:dataUser.data.user_id,
+                    username:data.username,
+                    role:data.role,
+                    profile_id:dataProfile.data.profile_id,
+                    token:token
+
+                }
               }
-              res.status(404).json(response);
+            //   res.status(404).json(response);
+              res.json(response);
         }
 
     }catch(error){
--- models/sql/userModel.js
+++ models/sql/userModel.js
@@ -1,55 +1,148 @@
-const { Sequelize } = require('sequelize');
-const sequelize = require('../../config/db_mysql');
+const { Sequelize } = require("sequelize");
+const sequelize = require("../../config/db_mysql");
 
 // Definisikan model User
-const User = sequelize.define('users', {
-    user_id:{
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    role: {
-      type: Sequelize.INTEGER,
-      allowNull: false
-    },
-    createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.NOW,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-  });
-  
+const User = sequelize.define("users", {
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  profile_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
+});
+
 // Fungsi untuk menampilkan User
 async function loginProses(username, password) {
-    try {
-        const users = await User.findOne({
-            where: {
-              username: username,
-              password: password,
-            },
-        });
-      if (users != null) {
-        return { status: "Sukses", message: "Login Berhasil!", data: users };
-      } else {
-        return { status: "Error", message: "Login Gagal!", data: users };
-      }
-    } catch (error) {
-      console.error(error);
-      return { status: "Error", message: "Terjadi Kesalahan!", data: error.message };
+  try {
+    const users = await User.findOne({
+      where: {
+        username: username,
+        password: password,
+      },
+    });
+    if (users != null) {
+      return { status: "Sukses", message: "Login Berhasil!", data: users };
+    } else {
+      return {
+        status: "Error",
+        message: "Pengguna Tidak Ditemukan!",
+        data: users,
+      };
     }
+  } catch (error) {
+    console.error(error);
+    return {
+      status: "Error",
+      message: "Terjadi Kesalahan Saat Login!",
+      data: error.message,
+    };
   }
-module.exports = { loginProses };
+}
+// Fungsi untuk menampilkan User
+async function loginOauth2(email) {
+  try {
+    const users = await User.findOne({
+      where: {
+        username: email,
+      },
+    });
+    if (users != null) {
+      return { status: "Sukses", message: "Login Berhasil!", data: users };
+    } else {
+      return {
+        status: "Error",
+        message: "Pengguna Tidak Ditemukan!",
+        data: users,
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      status: "Error",
+      message: "Terjadi Kesalahan Saat Login!",
+      data: error.message,
+    };
+  }
+}
+
+// Menyisipkan data baru
+async function insertUser(data) {
+  try {
+    const newUser = await User.create({
+      username: data.username,
+      password: data.password,
+      role: data.role,
+      profile_id: data.profile_id,
+    });
+
+    return {
+      status: "Sukses",
+      message: "Data user berhasil disisipkan!",
+      data: newUser,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      status: "Error",
+      message: "Terjadi Kesalahan saat menyisipkan data user!",
+      data: error.message,
+    };
+  }
+}
+
+// Memperbarui data
+async function updateUser(userId, data) {
+  try {
+    const updatedUser = await User.update(
+      {
+        username: data.username,
+        password: data.password,
+        role: data.role,
+      },
+      {
+        where: { user_id: userId },
+      }
+    );
+
+    if (updatedUser[0] > 0) {
+      return { status: "Sukses", message: "Data user berhasil diperbarui!" };
+    } else {
+      return { status: "Error", message: "Data user tidak ditemukan!" };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      status: "Error",
+      message: "Terjadi Kesalahan saat memperbarui data user!",
+      data: error.message,
+    };
+  }
+}
+
+module.exports = { loginProses, loginOauth2, insertUser, updateUser };
