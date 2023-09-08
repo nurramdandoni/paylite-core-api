7c8df8af612379d94139ab535032e84cfa9de05f
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-08 10:31:44 +0700

    add entity join siswa

--- models/sql/absensiModel.js
+++ models/sql/absensiModel.js
@@ -264,7 +264,7 @@ async function findAbsensiByWhere(whereData) {
     const tanggal_absensi_end = where.tanggal_absensi_end;
     try {
       const query = `
-      SELECT * FROM absensi WHERE absensi.lembaga_pendidikan_id='`+lembaga_pendidikan_id+`' AND absensi.jadwal_pelajaran_id='`+jadwal_pelajaran_id+`' AND absensi.siswa_id = '`+siswa_id+`' AND DATE(absensi.tanggal_absensi) BETWEEN '`+tanggal_absensi_start+`' AND '`+tanggal_absensi_end+`'; `;
+      SELECT * FROM absensi JOIN siswa ON absensi.siswa_id=siswa.siswa_id WHERE absensi.lembaga_pendidikan_id='`+lembaga_pendidikan_id+`' AND absensi.jadwal_pelajaran_id='`+jadwal_pelajaran_id+`' AND absensi.siswa_id = '`+siswa_id+`' AND DATE(absensi.tanggal_absensi) BETWEEN '`+tanggal_absensi_start+`' AND '`+tanggal_absensi_end+`'; `;
   
       const dataAbsensiUserTanggal = await sequelize.query(query, {
         // replacements: whereData,
@@ -293,7 +293,7 @@ async function findAbsensiByWhere(whereData) {
     const tanggal_absensi_end = where.tanggal_absensi_end;
     try {
       const query = `
-      SELECT * FROM absensi WHERE absensi.lembaga_pendidikan_id='`+lembaga_pendidikan_id+`' AND absensi.jadwal_pelajaran_id='`+jadwal_pelajaran_id+`'AND DATE(absensi.tanggal_absensi) BETWEEN '`+tanggal_absensi_start+`' AND '`+tanggal_absensi_end+`'; `;
+      SELECT * FROM absensi JOIN siswa ON absensi.siswa_id=siswa.siswa_id WHERE absensi.lembaga_pendidikan_id='`+lembaga_pendidikan_id+`' AND absensi.jadwal_pelajaran_id='`+jadwal_pelajaran_id+`'AND DATE(absensi.tanggal_absensi) BETWEEN '`+tanggal_absensi_start+`' AND '`+tanggal_absensi_end+`'; `;
   
       const dataAbsensiUserTanggal = await sequelize.query(query, {
         // replacements: whereData,
